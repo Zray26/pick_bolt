5a0b66b53e89eeb46e91750b9141677031ea8858
Zray26 <77483747+Zray26@users.noreply.github.com>
2021-08-19 18:24:26 +0800

    Add files via upload

---
+++ tf_cal.py
@@ -0,0 +1,93 @@
+rosrun tf tf_echo /base_link /left_ee_link
+At time 1629391205.927
+- Translation: [0.819, 0.154, 1.073]
+- Rotation: in Quaternion [0.721, -0.686, -0.028, 0.094]
+            in RPY (radian) [2.966, -0.090, -1.512]
+            in RPY (degree) [169.948, -5.140, -86.652]
+At time 1629391206.668
+- Translation: [0.819, 0.154, 1.073]
+- Rotation: in Quaternion [0.721, -0.686, -0.028, 0.094]
+            in RPY (radian) [2.966, -0.090, -1.512]
+            in RPY (degree) [169.948, -5.140, -86.652]
+At time 1629391207.666
+- Translation: [0.819, 0.154, 1.073]
+- Rotation: in Quaternion [0.721, -0.686, -0.028, 0.094]
+            in RPY (radian) [2.966, -0.090, -1.512]
+            in RPY (degree) [169.948, -5.140, -86.652]
+
+
+
+
+rosrun tf tf_echo /base_link /right_ee_link
+At time 1629391228.152
+- Translation: [0.811, -0.086, 1.077]
+- Rotation: in Quaternion [0.109, 0.022, 0.700, 0.706]
+            in RPY (radian) [0.187, -0.121, 1.551]
+            in RPY (degree) [10.719, -6.954, 88.842]
+At time 1629391228.909
+- Translation: [0.811, -0.086, 1.077]
+- Rotation: in Quaternion [0.109, 0.022, 0.700, 0.706]
+            in RPY (radian) [0.187, -0.121, 1.551]
+            in RPY (degree) [10.719, -6.954, 88.842]
+At time 1629391229.897
+- Translation: [0.811, -0.086, 1.077]
+- Rotation: in Quaternion [0.109, 0.022, 0.700, 0.706]
+            in RPY (radian) [0.187, -0.121, 1.551]
+            in RPY (degree) [10.719, -6.954, 88.842]
+
+
+
+
+/base_link /ar_marker_3
+At time 1629391269.363
+- Translation: [0.718, -0.008, 1.009]
+- Rotation: in Quaternion [-0.366, -0.515, 0.627, 0.455]
+            in RPY (radian) [-1.368, -0.009, 1.893]
+            in RPY (degree) [-78.388, -0.518, 108.475]
+At time 1629391270.364
+- Translation: [0.718, -0.008, 1.009]
+- Rotation: in Quaternion [-0.365, -0.515, 0.628, 0.455]
+            in RPY (radian) [-1.366, -0.010, 1.896]
+            in RPY (degree) [-78.278, -0.599, 108.657]
+At time 1629391271.361
+- Translation: [0.717, -0.008, 1.010]
+- Rotation: in Quaternion [-0.365, -0.516, 0.628, 0.455]
+            in RPY (radian) [-1.367, -0.011, 1.897]
+            in RPY (degree) [-78.337, -0.633, 108.707]
+
+
+
+/ar_marker_3   /right_ee_link
+- Translation: [-0.103, -0.081, -0.048]
+- Rotation: in Quaternion [0.682, 0.051, -0.173, 0.709]
+            in RPY (radian) [1.502, 0.313, -0.186]
+            in RPY (degree) [86.081, 17.906, -10.664]
+At time 1629391323.366
+- Translation: [-0.103, -0.081, -0.048]
+- Rotation: in Quaternion [0.683, 0.050, -0.173, 0.708]
+            in RPY (radian) [1.504, 0.311, -0.186]
+            in RPY (degree) [86.184, 17.827, -10.678]
+At time 1629391324.357
+- Translation: [-0.103, -0.081, -0.048]
+- Rotation: in Quaternion [0.682, 0.049, -0.173, 0.709]
+            in RPY (radian) [1.503, 0.311, -0.187]
+            in RPY (degree) [86.130, 17.802, -10.742]
+
+
+/ar_marker_3   /left_ee_link
+- Translation: [0.121, -0.091, -0.132]
+- Rotation: in Quaternion [0.082, 0.706, 0.694, -0.115]
+            in RPY (radian) [1.582, -0.280, -3.096]
+            in RPY (degree) [90.648, -16.066, -177.374]
+At time 1629391295.380
+- Translation: [0.121, -0.091, -0.132]
+- Rotation: in Quaternion [0.082, 0.706, 0.694, -0.116]
+            in RPY (radian) [1.582, -0.281, -3.096]
+            in RPY (degree) [90.645, -16.100, -177.360]
+At time 1629391296.333
+- Translation: [0.122, -0.091, -0.132]
+- Rotation: in Quaternion [0.082, 0.705, 0.695, -0.115]
+            in RPY (radian) [1.580, -0.279, -3.096]
+            in RPY (degree) [90.523, -15.976, -177.375]
+
+
